7886de67b31ca82faf66915c4e8c066b74330e54
kimberly esekody <kimberly.esekody@pau.edu.com>
2023-04-04 14:31:24 +0100

    (my second cmmit)

---
+++ Week-1/Untitled.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Integer number 
+num = 100
+print(num)
+print("Data Type of variable num is", type(num))
+
+# float number
+fnum = 34.45
+print(fnum)
+print("Data Type of variables fnum is", type(fnum))
+
+# complex number
+cnum = 3 + 4j
+print(cnum)
+print("Data Type of variable cnum is", type(cnum))
+
+
+# In[ ]:
+
+
+
+
